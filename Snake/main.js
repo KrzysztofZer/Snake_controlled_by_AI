c53754f623133459b577e7876db67979d0f27086
KrzysztofZer <krzysiekzerman@gmail.com>
2018-04-26 19:29:05 +0200

    Random food

--- Snake/main.js
+++ Snake/main.js
@@ -1,27 +1,23 @@
 var offsetX = 15;
 var offsetY = 15;
 var diameter = 30;
+var score = 0;
 
 var snake;
-var wall1;
+var wall;
 
 function setup(){
 	createCanvas(640, 480);
-  //  ellipse(offsetX, offsetY, 30, 30);
     snake = createSprite(offsetX,offsetY,diameter,diameter);
-    wall1 = createSprite(105,15,diameter,30);
+    createWall();
     snake.shadowColor = color(255)
     fill(255);
-
 }
+
 function draw(){
         background(50);
-
-
-   
+    text(score,620,15);   
     drawSprites();
-
-
 }
 
 function keyPressed(){
@@ -29,30 +25,33 @@ if (keyCode === RIGHT_ARROW && offsetX<=610){
 offsetX = offsetX+30;
 snake.position = createVector(offsetX, offsetY);
 checkValidation();
-
 }
 
 if (keyCode === LEFT_ARROW && offsetX>=30){
     offsetX = offsetX-30;
 snake.position = createVector(offsetX, offsetY);
+    checkValidation();
 }
     
 if (keyCode === DOWN_ARROW && offsetY<=450){
     offsetY = offsetY+30;
 snake.position = createVector(offsetX,offsetY);
+    checkValidation();
 }
     
 if (keyCode === UP_ARROW && offsetY>=30){
         offsetY = offsetY-30;
 snake.position = createVector(offsetX,offsetY);
-}
-}
+    checkValidation();
+}}
 
 function checkValidation (){
-//    print(snake.position.x);
-//    print(wall1.position.x);
-       if (snake.position.x === wall1.position.x){
-           print("test");
-           wall1.remove();
-       }           
+       if (snake.position.x <= wall.position.x+30&&snake.position.x >= wall.position.x-30&&snake.position.y <= wall.position.y+30&&snake.position.y >= wall.position.y-30){
+           wall.remove();
+           score = score+1;
+           createWall();
+}}
+
+function createWall (){
+        wall = createSprite(Math.floor((Math.random() * 590) + 30),Math.floor((Math.random() * 430) + 30),diameter,30);
 }
